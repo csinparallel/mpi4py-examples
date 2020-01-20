54c709e52591bd90bd4d40c86cd096ac38801f6e
Dick Brown <rab@stolaf.edu>
2020-01-20 17:42:15 -0600

    rename and revise dd_mpi_slices.py to use dd_functions.py, include timings

--- drug-design/dd_mpi_slices.py
+++ drug-design/dd_mpi_slices.py
@@ -5,49 +5,19 @@
 
 import string
 import argparse
-import random
 import math
 from mpi4py import MPI
+
+from dd_functions import *
 
 DFLT_maxLigand = 5
 DFLT_nLigands = 120
 DFLT_protein = "the cat in the hat wore the hat to the cat hat party"
 
-# function makeLigand
-#   1 argument:  maximum length of a ligand
-#   return:  a random ligand string of random length between 1 and arg1
-
-def makeLigand(maxLength):
-    len = random.randint(1, maxLength)
-    ligand = ""
-    for c in range(len):
-        ligand = ligand + string.ascii_lowercase[random.randint(0,25)]
-    return ligand
-
-# function score
-#   2 arguments:  a ligand and a protein sequence
-#   return:  int, simulated binding score for ligand arg1 against protein arg2
-
-def score(lig, pro):
-    if len(lig) == 0 or len(pro) == 0:
-        return 0
-    if lig[0] == pro[0]:
-        return 1 + score(lig[1:], pro[1:])
-    else:
-        return max(score(lig[1:], pro), score(lig, pro[1:]))
-
-# function printIf - used for verbose output
-#   variable number of arguments:  a boolean, then valid arguments for print
-#   state change:  if arg1 is True, call print with the remaining arguments
-
-def printIf(cond, *positionals, **keywords):
-    if cond:
-        print(*positionals, **keywords)
-
 # main program
 
 def main():
-    random.seed(0) # guarantees that each run uses same random number sequence
+    start = comm.Wtime()
 
     # parse command-line args...
     parser = argparse.ArgumentParser(
@@ -74,7 +44,8 @@ def main():
 
     # assert - numProcesses > 1
     if id == 0:    # master
-   
+        random.seed(0) # guarantees each run uses same random number sequence
+
         # generate ligands
         ligands = []
         for l in range(args.nLigands):
@@ -89,7 +60,10 @@ def main():
             comm.send(ligands[(p-1)*n:p*n], dest=p)
 
         for p in range(1, numProcesses):
-            received = comm.recv(source=p)
+            stat = MPI.Status()
+            received = comm.recv(source=MPI.ANY_SOURCE, status=stat)
+            w = stat.Get_source()
+
             if received[0] > maxScore:
                 maxScore = received[0]
                 maxScoreLigands = received[1]
@@ -99,9 +73,11 @@ def main():
         # print results
         print('The maximum score is', maxScore)
         print('Achieved by ligand(s)', maxScoreLigands)
+        finish = comm.Wtime()
+        print('Overall time is', finish - start)
 
     else:       # worker
-    
+
         ligandList = comm.recv(source=0)
         maxScore = -1
         maxScoreLigands = []
@@ -122,5 +98,7 @@ def main():
         
         printIf(args.verbose)  # print final newline
         comm.send([maxScore, maxScoreLigands], dest=0)
+        finish = comm.Wtime()
+        print('[{}] time is {}'.format(id, finish - start))
 
 main()
