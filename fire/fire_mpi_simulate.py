7e8012cdf5e11a566585cf0ac723f156cd1aaf36
Libby Shoop <shoop@macalester.edu>
2020-01-19 22:38:23 -0600

    added seed of random to mpi version and imporved appearance of plots

--- fire/fire_mpi_simulate.py
+++ fire/fire_mpi_simulate.py
@@ -8,64 +8,15 @@
 #
 # Libby Shoop     Macalester College
 #
-import argparse      # for command-line arguments
+
 import matplotlib.pyplot as plt
 import math
 import time
 from mpi4py import MPI
 
 from fire_functions import *
+from sim_functions import *
 
-def parseArguments():
-    """Handle command line arguments
-
-    Run with -h to get details of each argument.
-
-    Returns:
-        A list containg each argument provided
-    """
-    # process command line arguments
-    # see https://docs.python.org/3.3/howto/argparse.html#id1
-    parser = argparse.ArgumentParser()
-    parser.add_argument("numTreesPerRow", help="number of trees in row of square grid")
-    parser.add_argument("probabilityIncrement", help="amount to increment the probability threshold of fire spreading for each set of probability trials")
-    parser.add_argument("numberOfTrials", help="number of times to run the fire simulation with a new forest for each proability in set of probabilities")
-
-    args = parser.parse_args()
-
-    row_size = int(args.numTreesPerRow)
-    prob_spread_increment = float(args.probabilityIncrement)
-    num_trials = int(args.numberOfTrials)
-
-    return [row_size, prob_spread_increment, num_trials]
-
-
-def burn_until_out(row_size, forest, prob_spread):
-    """ one simulation of the buring forest
-    Parameters:
-        row_size (int): number of trees in each row and column
-        forest (array): array representing the 2D forest
-        prob_spread (float):
-            probability threshold for determining whether burning tree will
-            spread to neighboring tree
-    """
-
-    percent_burned = 0.0
-    # for now start burning at midlle tree
-    middle_tree_index = int(row_size/2)
-    light_tree(row_size, forest, middle_tree_index, middle_tree_index)
-
-    iter = 0 # how many iterations before the fire burns out
-    while forest_is_burning(forest):
-        # print("burning") # debug
-        forest_burns(forest, row_size, prob_spread)
-        iter += 1
-
-    percent_burned = get_percent_burned(forest, row_size)
-
-    # print_forest(forest)  #debug
-
-    return int(iter), float(percent_burned)
 
 ############################# main() ##########################
 def main():
@@ -75,6 +26,7 @@ def main():
     numProcesses = comm.Get_size()  #total number of processes running
     myHostName = MPI.Get_processor_name()  #machine name running the code
 
+    np.random.seed()    # each process starts with seed from its /dev/urandom
 
     start = MPI.Wtime() # start the timing
     # master process will get the arguments
@@ -171,14 +123,17 @@ def main():
         upper_title = "Simulation: {0} trials for each probability\n {1}x{1} forest\nRun time on {2} processes: {3:12.4f} seconds"
         upper_title = upper_title.format(tot_num_trials, row_size,  numProcesses, total_time)
 
-        fig, (ax1, ax2) = plt.subplots(1, 2)  # 2 plots side by side
+        # 2 plots side by side
+        fig, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={'wspace': 0.24}, figsize=(8.5,6))
         fig.canvas.set_window_title("Forest Fire Simulation with Multiple Trials in Parallel")
+        print("figure width: {}".format(fig.get_figwidth()))
+        # fig.set_figwidth(fig.get_figwidth() + 1.0, forward=True)
         fig.suptitle(upper_title)
 
-        ax1.plot(percent_burned_data[:,0], percent_burned_data[:,1])
+        ax1.plot(percent_burned_data[:,0], percent_burned_data[:,1], linestyle='-', marker='.', color='tab:blue')
         ax1.set(xlabel="Probability threshold", ylabel="Avg percent burned")
 
-        ax2.plot(iters_per_sim_data[:,0], iters_per_sim_data[:,1])
+        ax2.plot(iters_per_sim_data[:,0], iters_per_sim_data[:,1], linestyle='-', marker='.', color='xkcd:maroon')
         ax2.set(xlabel="Probability threshold", ylabel="Avg iterations per simulation")
 
         plt.show()
--- fire/fire_sequential_simulate.py
+++ fire/fire_sequential_simulate.py
@@ -12,57 +12,8 @@ import math
 import time
 
 from fire_functions import *
+from sim_functions import *
 
-def parseArguments():
-    """Handle command line arguments
-
-    Run with -h to get details of each argument.
-
-    Returns:
-        A list containg each argument provided
-    """
-    # process command line arguments
-    # see https://docs.python.org/3.3/howto/argparse.html#id1
-    parser = argparse.ArgumentParser()
-    parser.add_argument("numTreesPerRow", help="number of trees in row of square grid")
-    parser.add_argument("probabilityIncrement", help="amount to increment the probability threshold of fire spreading for each set of probability trials")
-    parser.add_argument("numberOfTrials", help="number of times to run the fire simulation with a new forest for each proability in set of probabilities")
-
-    args = parser.parse_args()
-
-    row_size = int(args.numTreesPerRow)
-    prob_spread_increment = float(args.probabilityIncrement)
-    num_trials = int(args.numberOfTrials)
-
-    return row_size, prob_spread_increment, num_trials
-
-
-def burn_until_out(row_size, forest, prob_spread):
-    """ one simulation of the buring forest
-    Parameters:
-        row_size (int): number of trees in each row and column
-        forest (array): array representing the 2D forest
-        prob_spread (float):
-            probability threshold for determining whether burning tree will
-            spread to neighboring tree
-    """
-
-    percent_burned = 0.0
-    # for now start burning at midlle tree
-    middle_tree_index = int(row_size/2)
-    light_tree(row_size, forest, middle_tree_index, middle_tree_index)
-
-    iter = 0 # how many iterations before the fire burns out
-    while forest_is_burning(forest):
-        # print("burning") # debug
-        forest_burns(forest, row_size, prob_spread)
-        iter += 1
-
-    percent_burned = get_percent_burned(forest, row_size)
-
-    # print_forest(forest)  #debug
-
-    return int(iter), float(percent_burned)
 
 ############################# main() ##########################
 def main():
@@ -114,14 +65,15 @@ def main():
     upper_title = "Simulation: {0} trials for each probability\n {1}x{1} forest\nRun time on 1 process: {2:12.4f} seconds"
     upper_title = upper_title.format(num_trials, row_size, total_time)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)  # 2 plots side by side
+    # 2 plots side by side
+    fig, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={'wspace': 0.24}, figsize=(8.5,6))
     fig.canvas.set_window_title("Forest Fire Simulation with Multiple Trials (1 process)")
     fig.suptitle(upper_title)
 
-    ax1.plot(percent_burned_data[:,0], percent_burned_data[:,1])
+    ax1.plot(percent_burned_data[:,0], percent_burned_data[:,1], linestyle='-', marker='.', color='tab:blue')
     ax1.set(xlabel="Probability threshold", ylabel="Avg percent burned")
 
-    ax2.plot(iters_per_sim_data[:,0], iters_per_sim_data[:,1])
+    ax2.plot(iters_per_sim_data[:,0], iters_per_sim_data[:,1], linestyle='-', marker='.', color='xkcd:maroon')
     ax2.set(xlabel="Probability threshold", ylabel="Avg iterations per simulation")
 
     plt.show()
